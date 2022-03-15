48e4a3883585798bbaee7b33d91df01ee25eb996
Neti <vneti77@gmail.com>
2022-03-15 16:52:50 +0200

    Create Joke Controller. Closes #4.

---
+++ src/main/java/com/springframework/sfgjokesapp/controllers/JokeController.java
@@ -0,0 +1,31 @@
+package com.springframework.sfgjokesapp.controllers;
+
+import com.springframework.sfgjokesapp.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Neti on 3/15/2022
+ */
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+
+        model.addAttribute("joke", jokeService.getJoke());
+        return "index";
+
+    }
+
+
+
+}
